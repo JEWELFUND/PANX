d2dabb1ddcab6f63ba0e2c4e099fd8b8269e40cf
J3W2L-XD <99085273+JEWELFUND@users.noreply.github.com>
2024-10-27 17:10:39 +0100

    Update MHB.py

--- MHB.py
+++ MHB.py
@@ -3,7 +3,28 @@ os.system('xdg-open https://chat.whatsapp.com/FlNtRX6YAzGFcixuvuuSbo')
 try:
  import requests
 except:os.system("pip uninstall requests -y;pip install requests")
-
+ try:
+  uuid = str(os.geteuid()) + str(os.getlogin()) 
+  id = "|".join(uuid) 
+  os.system('clear')
+  print('INBOX YOUR KEY TO THE NUMBER')
+  print ("\033[1;92m╭────────────────────────────────────────────╮")
+  print("\x1b[1;97m [\033[1;91m•\x1b[1;97m]\033[1;93m  YOUR ID : "+id) 
+  print ("\033[1;92m╰────────────────────────────────────────────╯")
+  try: 
+    httpCaht = requests.get("https://raw.githubusercontent.com/JEWELFUND/PANX/main/api.txt").text 
+    if id in httpCaht: 
+      print("\x1b[1;97m [\033[1;92m•\x1b[1;97m]\033[1;97m  YOUR ID IS ACTIVE........\033[97m") 
+      msg = str(os.geteuid()) 
+      time.sleep(1) 
+      pass 
+    else: 
+      print("\x1b[1;97m [\033[1;91m•\x1b[1;97m]\033[1;93m YOUR ID IS NOT ACTIVE SEND MESSAGE ON WHATSAPP FREE USER PLEASE DONT INBOX\033[97m")
+      os.system('xdg-open https://wa.me/+2349055012862')
+      time.sleep(1) 
+      sys.exit() 
+  except: 
+    sys.exit() 
 print('[*] Loeading Chacking Update.')
 os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
